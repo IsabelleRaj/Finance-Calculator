76d66b85b95ac3344b7a69aa4d3d4b3c48035385
Isabelle Rajendiran <isabellerajendiran@ymail.com>
2023-12-10 13:27:34 +0000

    Update finance_calculator_improved.py
    Improved code style to adhere to PEP8 guidelines

--- finance_calculator_improved.py
+++ finance_calculator_improved.py
@@ -1,5 +1,5 @@
 """
-This is a Python program allowing the user to access two different financial 
+This is a Python program allowing the user to access two different financial
 calculators: an investment calculator and a home loan repayment calculator.
 """
 
@@ -16,13 +16,14 @@ while carry_on:
           \n> bond       - to calculate the amount you'll have to pay on a home loan
           \n===================================================================================
           """)
-    choice = input("Enter either 'investment' or 'bond' from the menu above to proceed.\nTo exit the program, enter 'quit': ").lower()
+    choice = input("""Enter either 'investment' or 'bond' from the menu above to proceed.
+                   \nTo exit the program, enter 'quit': """).lower()
 
     # Proceed according to the user's chosen type of calculator
     if choice == 'investment':
         # Request and store user inputs
         while True:
-            try: 
+            try:
                 # P = The amount deposited
                 P = float(input("\nEnter the amount of money you are depositing (omit £ symbol): "))
                 # r = Rate of interest (%)
@@ -51,12 +52,12 @@ while carry_on:
     elif choice == 'bond':
         # Request and store user inputs
         while True:
-            try: 
+            try:
                 # P = Present value of the house
                 P = float(input("\nEnter the present value of the house (omit £ symbol): "))
                 # i = Monthly interest rate
                 i = float(input("Enter your yearly interest rate (omit % symbol): "))/100
-                i = i/12 
+                i = i/12
                 # n = The number of months over which the bond will be repaid
                 n = float(input("Enter the amount of months you plan to repay the bond: "))
                 break
@@ -65,7 +66,7 @@ while carry_on:
         # Calculate monthly repayment on home loan
         repayment = round((i*P)/(1 - (1+i)**(-n)),2)
         print(f"\nYou will have to repay £{repayment} each month.")
-    
+
     elif choice == 'quit':
         carry_on = False
 
